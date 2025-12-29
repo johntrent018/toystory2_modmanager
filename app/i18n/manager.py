ce32264641350e8a3788d9e3c9f2f1b2c41500b0
John Trent <johntrent018@gmail.com>
2025-12-29 17:53:23 +0100

    Refactorisation de la structure des fichiers de langue pour mieux structurer les chaînes de caractères et les regrouper par onglet et sous-onglet de l'application. Cela permet également de plus facilement coder des boucles for et autres dans les méthodes refresh pour éviter la répétition de code forcée par des clés en dur dans les fichiers de langue.

--- app/i18n/manager.py
+++ app/i18n/manager.py
@@ -1,44 +1,83 @@
+# Importations
 import json
 import os
 
 class LanguageManager:
-    
+
     # Constructeur
-    def __init__(self, locales_path, default_lang):
+    def __init__(self, locales_path, default_lang="en-GB"):
 
-        self.locales = {}
-        self.lang = default_lang
-        self._observers = []
+        self.locales_path = locales_path
+        self.default_lang = default_lang
+        self.current_lang = default_lang
 
-        for file in os.listdir(locales_path):
+        self.translations = {}
+        self.observers = []
+
+        self._load_languages()
+
+    # -----------------------
+    # Chargement des langues
+    # -----------------------
+
+    def _load_languages(self):
+        for file in os.listdir(self.locales_path):
             if file.endswith(".json"):
-                with open(os.path.join(locales_path, file), encoding="utf-8") as f:
-                    data = json.load(f)
-                    self.locales[data["meta"]["code"]] = data
+                lang_code = file.replace(".json", "")
+                file_path = os.path.join(self.locales_path, file)
+
+                with open(file_path, "r", encoding="utf-8") as f:
+                    self.translations[lang_code] = json.load(f)
+
+    # -----------------------
+    # Design Pattern "Observeur"
+    # -----------------------
+
+    def register(self, callback):
+        if callback not in self.observers:
+            self.observers.append(callback)
+
+    def notify(self):
+        for callback in self.observers:
+            callback()
+
+    # -----------------------
+    # Gestion de la langue
+    # -----------------------
+
+    def set_language(self, lang_code):
+        if lang_code in self.translations:
+            self.current_lang = lang_code
+            self.notify()
 
     def available_languages(self):
-        return list(self.locales.keys())
+        return list(self.translations.keys())
 
     def language_name(self, lang_code, target_lang=None):
         if target_lang is None:
-            target_lang = self.lang
-        return self.locales[lang_code]["meta"]["name"].get(
+            target_lang = self.current_lang
+        return self.translations[lang_code]["meta"]["name"].get(
             target_lang,
             lang_code
         )
 
-    def translate(self, key):
-        return self.locales[self.lang].get(key, key)
+    # -----------------------
+    # Traduction (CLÉ DU SYSTÈME)
+    # -----------------------
 
-    def set_language(self, lang):
-        if lang in self.locales and lang != self.lang:
-            self.lang = lang
-            self._notify()
+    def translate(self, key_path, default=None):
+        
+        # key_path : str avec notation par points (ex: "app_data.tabs.create_mod.sub_tabs.music_sfx.h1")
+        # Retourne soit une string, soit une liste, soit un dict, ou 'default' (ou key_path) si introuvable
+        data = self.translations.get(self.current_lang)
+        if not data:
+            return default if default is not None else key_path
+        keys = key_path.split(".")
+        value = data
 
-    def register(self, callback):
-        self._observers.append(callback)
-
-    # Méthode pour envoyer une notification aux observers
-    def _notify(self):
-        for cb in self._observers:
-            cb()
+        try:
+            for key in keys:
+                value = value[key]
+            return value
+        except (KeyError, TypeError):
+            return default if default is not None else key_path
