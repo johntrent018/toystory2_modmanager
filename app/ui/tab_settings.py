780fc2b9dfd274cc61d0acc2484e7c7c783c35bc
John Trent <johntrent018@gmail.com>
2025-12-30 00:37:16 +0100

    Remise en forme du fichier pour commencer à implémenter les widgets du gamepath.

--- app/ui/tab_settings.py
+++ app/ui/tab_settings.py
@@ -1,18 +1,36 @@
+# Importations de base
 import tkinter as tk
 from tkinter import ttk
+# Importation de composants
 from preferences import Preferences
 
 class SettingsTab(ttk.Frame):
+
+    # Constructeur
     def __init__(self, parent, lang, prefs):
+
         super().__init__(parent)
 
+        # --------------------------------
+        # Partie "GamePath" de l'interface
+        # --------------------------------
+
+        
+
+        # ------------------------------
+        # Partie "Langue" de l'interface
+        # ------------------------------
+
+        # Propriétés et variables liées à la partie Langue des paramétrages
         self.lang = lang
         self.prefs = prefs
         self.selected = tk.StringVar()
 
+        # Label avant la liste déroulante des langues
         self.label = ttk.Label(self)
         self.label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
+        # Liste déroulante du choix de la langue
         self.combo = ttk.Combobox(
             self,
             state="readonly",
@@ -20,6 +38,7 @@ class SettingsTab(ttk.Frame):
         )
         self.combo.grid(row=0, column=1, padx=10, pady=10)
 
+        # Bouton "Appliquer" du choix de la langue
         self.button = ttk.Button(self, command=self.apply)
         self.button.grid(row=1, column=0, columnspan=2, pady=10)
 
